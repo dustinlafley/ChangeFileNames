6f989a45540b5514cff01d6c3e0952397ff08803
dustinlafley <dustin.lafley@gmail.com>
2025-12-28 16:48:30 -0600

    changed the path to the python file

--- ChnageFileNames.bat
+++ ChnageFileNames.bat
@@ -5,5 +5,5 @@ set /p DRY="Dry run? (y/n): "
 set DRY_ARG=
 if /I "%DRY%"=="y" set DRY_ARG=--dry-run
 
-py "C:\Users\chase\Documents\roms\GC Games" --source "%SRC%" --dest "%DST%" --ext ".iso"  %DRY_ARG%
+py "C:\Users\chase\Documents\Python Projects\ChangeFileNames.py" --source "%SRC%" --dest "%DST%" --ext ".iso"  %DRY_ARG%
 pause
